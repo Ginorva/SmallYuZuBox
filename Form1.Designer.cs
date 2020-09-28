8f943318899c97f6e1543972d409ad9dc73b1e90
张庭恺 <2859644531@qq.com>
2020-09-28 11:10:02 +0800

    首次提交项目

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SmallYuzuBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(164, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 12);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 14);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "股票码-买入价-操作";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("宋体", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(128, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 18);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "DO";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.name,
+            this.Column1,
+            this.Column2,
+            this.rate});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(164, 208);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "代码";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 54;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "股票名称";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 78;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "买入价";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 66;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "现价";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 54;
+            // 
+            // rate
+            // 
+            this.rate.HeaderText = "涨跌比率";
+            this.rate.Name = "rate";
+            this.rate.ReadOnly = true;
+            this.rate.Width = 78;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(179, 246);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "小柚子盒子";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rate;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmallYuzuBox
+{
+    public partial class Form1 : Form
+    {
+        private bool isMouse = false; // 鼠标是否按下
+        // 原点位置
+        private int originX = 0;
+        private int originY = 0;
+        // 鼠标按下位置
+        private int mouseX = 0;
+        private int mouseY = 0;
+        Boolean textboxHasText = false;//判断输入框是否有文本
+        public Form1()
+        {
+            InitializeComponent();             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(255,192,128);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;         
+            Font font = new Font(dataGridView1.ColumnHeadersDefaultCellStyle.Font, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = font;            
+            dataGridView1.BackgroundColor = Color.FromArgb(255, 192, 128);
+            dataGridView1.AllowUserToResizeRows = false;           
+            string basepath = "db";
+            string path = basepath + @"\base.sqlite";
+            SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+            cn.Open();
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.Connection = cn;
+            if (!Directory.Exists(basepath))
+            {
+                Directory.CreateDirectory(basepath);                                           
+                cmd.CommandText = "create table if not exists user_own_code(code varchar(20),bought numeric(9,2))";
+                cmd.ExecuteNonQuery();               
+            }                                           
+            cn.Close();
+            Refresh_Data();
+        }
+        
+        public void Refresh_Data() {
+            dataGridView1.Rows.Clear();
+            string basepath = "db";
+            string path = basepath + @"\base.sqlite";
+            SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+            cn.Open();
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = "select * from user_own_code";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            DataTable db = new DataTable();
+            adapter.Fill(db);
+            WebClient client = new WebClient();
+            string request = string.Empty;
+            foreach (DataRow r in db.Rows)
+            {
+                request += r[0] + ",";
+            }
+            string url = "http://hq.sinajs.cn/list=" + request;
+            String sr = client.DownloadString(url);
+            sr = sr.Replace("\n", "");
+            String[] srS = sr.Split(';');
+            for (int i = 0; i < srS.Length; i++)
+            {
+                if (srS[i] != string.Empty)
+                {
+                    String[] srs = srS[i].Split(',');
+                    int index = dataGridView1.Rows.Add();
+                    String code = srs[0].Substring(11, srs[0].IndexOf('=') - 11);
+                    dataGridView1.Rows[index].Cells[0].Value = code;
+                    dataGridView1.Rows[index].Cells[2].Value = db.Rows[i][1];
+                    if (code.ToLower().StartsWith("sh") || code.ToLower().StartsWith("sz"))
+                    {
+                        String[] srss = srs[0].Split('"');
+                        dataGridView1.Rows[index].Cells[1].Value = srss[1];
+                        dataGridView1.Rows[index].Cells[3].Value = srs[3];
+                        double rate = Math.Round(((Double.Parse(srs[3]) - Double.Parse(db.Rows[i][1].ToString())) / Double.Parse(db.Rows[i][1].ToString())) * 100, 2, MidpointRounding.AwayFromZero);
+                        dataGridView1.Rows[index].Cells[4].Value = rate > 0 ? "+ " + Math.Abs(rate) + "%" : "- " + Math.Abs(rate) + "%";
+                        dataGridView1.Rows[index].Cells[4].Style.ForeColor = rate > 0 ? Color.Red : Color.Green;
+                    }
+                    else if (code.ToLower().StartsWith("us"))
+                    {
+                        dataGridView1.Rows[index].Cells[1].Value = srs[0];
+                        dataGridView1.Rows[index].Cells[3].Value = srs[1];
+                        double rate = Math.Round(((Double.Parse(srs[1]) - Double.Parse(db.Rows[i][1].ToString())) / Double.Parse(db.Rows[i][1].ToString())) * 100, 2, MidpointRounding.AwayFromZero);
+                        dataGridView1.Rows[index].Cells[4].Value = rate > 0 ? "+ " + Math.Abs(rate) + "%" : "- " + Math.Abs(rate) + "%";
+                        dataGridView1.Rows[index].Cells[4].Style.ForeColor = rate > 0 ? Color.Red : Color.Green;
+                    }
+                    else if (code.ToLower().StartsWith("hk"))
+                    {
+                        dataGridView1.Rows[index].Cells[1].Value = srs[1];
+                        dataGridView1.Rows[index].Cells[3].Value = srs[6];
+                        double rate = Math.Round(((Double.Parse(srs[6]) - Double.Parse(db.Rows[i][1].ToString())) / Double.Parse(db.Rows[i][1].ToString())) * 100, 2, MidpointRounding.AwayFromZero);
+                        dataGridView1.Rows[index].Cells[4].Value = rate > 0 ? "+ " + Math.Abs(rate) + "%" : "- " + Math.Abs(rate) + "%";
+                        dataGridView1.Rows[index].Cells[4].Style.ForeColor = rate > 0 ? Color.Red : Color.Green;
+                    }
+                }
+            }
+            cn.Close();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouse)
+            {
+                // 移动距离
+                int moveX = (e.X + this.Location.X) - mouseX;
+                int moveY = (e.Y + this.Location.Y) - mouseY;
+                int targetX = originX + moveX;
+                int targetY = originY + moveY;
+                this.Location = new Point(targetX, targetY);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            { // 判断鼠标按键
+                isMouse = true;
+                // 屏幕坐标位置
+                originX = this.Location.X;
+                originY = this.Location.Y;
+                // 鼠标按下位置
+                mouseX = originX + e.X;
+                mouseY = originY + e.Y;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isMouse)
+            {
+                isMouse = false;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+ 
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textboxHasText == false)
+                textBox1.Text = "";
+
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "股票码-买入价-操作";
+                textBox1.ForeColor = Color.LightGray;
+                textboxHasText = false;
+            }
+            else
+                textboxHasText = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {                 
+            Refresh_Data();           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string basepath = "db";
+            string path = basepath + @"\base.sqlite";
+            SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+            try
+            {
+                if (textBox1.Text == "股票码-买入价-操作")
+                {
+                    MessageBox.Show("请输入内容");
+                    return;
+                }
+                if (textBox1.Text.EndsWith("D"))
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.Connection = cn;
+                    String[] inputs = textBox1.Text.Split('-');
+                    cmd.CommandText = "delete from user_own_code where code='" + inputs[0] + "'";
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    Refresh_Data();
+                    MessageBox.Show("删除成功");
+                }
+                else if (textBox1.Text.EndsWith("U"))
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.Connection = cn;
+                    String[] inputs = textBox1.Text.Split('-');
+                    cmd.CommandText = "update user_own_code set bought=" + inputs[1] + " where code='" + inputs[0] + "'";
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    Refresh_Data();
+                    MessageBox.Show("修改成功");
+                }
+                else if (textBox1.Text.EndsWith("A"))
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.Connection = cn;
+                    String[] inputs = textBox1.Text.Split('-');
+                    cmd.CommandText = "insert into user_own_code values('" + inputs[0] + "'," + inputs[1] + ")";
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                    Refresh_Data();
+                    MessageBox.Show("添加成功");
+                }
+                else {
+                    MessageBox.Show("请输入有效指令");
+                }
+            }
+            catch {
+                MessageBox.Show("请输入正确的指令");
+            }
+        }
+    }
+}
